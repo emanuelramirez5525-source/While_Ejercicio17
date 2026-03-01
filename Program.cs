ca59e6516a7c37f80b0a12bc6650be28fd74ee51
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 20:30:43 -0600

    Logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿//17. Contar cuántos números positivos ingresa el usuario.
+int numero = 1;
+int total = 0;
+
+while (numero != 0)
+{
+    Console.WriteLine("Ingrese número positivos (presione 0 para salir)");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        total++;
+    }
+
+}
+Console.WriteLine("El total de números ingresados es en total: " + total);
